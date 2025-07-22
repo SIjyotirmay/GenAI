a68369473acc3bd969d4d8d4096d2d6c52af03f3
Jyotirmay Shrestha <jyotirmayshrestha7@gmail.com>
2025-07-22 14:51:37 +0530

    day2

--- 2.ChatModels/5_hf_local.py
+++ 2.ChatModels/5_hf_local.py
@@ -1,27 +1,29 @@
-# from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
-
-# llm = HuggingFacePipeline(
-#     repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-#     task ="text-generation",
-#     pipeline_kwargs=dict(
-#         temperture=0.5,
-#         max_new_tokens=100
-#     )
-# )
-
-# model = ChatHuggingFace(llm=llm)
-
-from transformers import pipeline
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 
-# Build the HF Transformers pipeline manually
-pipe = pipeline(
-    task="text-generation",
-    model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    temperature=0.5,
-    max_new_tokens=100
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task ="text-generation",
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
 )
 
-# Wrap in LangChain
-llm = HuggingFacePipeline(pipeline=pipe)
 model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of France?")
+print(result.content)
+# from transformers import pipeline
+# from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+
+# # Build the HF Transformers pipeline manually
+# pipe = pipeline(
+#     task="text-generation",
+#     model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+#     temperature=0.5,
+#     max_new_tokens=100
+# )
+
+# # Wrap in LangChain
+# llm = HuggingFacePipeline(pipeline=pipe)
+# model = ChatHuggingFace(llm=llm)
